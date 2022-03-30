70628dff8ee79cb0afcb1a00a7e2184020870e67
P1 <jack@garzafamily.org>
2022-03-30 09:39:15 -0700

    Changes

--- webapp.py
+++ webapp.py
@@ -6,6 +6,14 @@ import json
 
 app = Flask(__name__)
 
+@app.route("/")
+def render_main():
+    return render_template('index.html')
 
+@app.route("/p1")
+def render_page1():
+    return render_template('page1.html')
+
+    
 if __name__ == '__main__':
     app.run(debug=False) # change to False when running in production
