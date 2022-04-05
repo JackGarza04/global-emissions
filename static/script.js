b80867012bba3607a926e6d27348a0986598d749
P1 <jack@garzafamily.org>
2022-04-05 09:51:26 -0700

    Added second graph

--- static/script.js
+++ static/script.js
@@ -1,3 +1,12 @@
-window.onload = function () {
-
+$(document).ready(function() {
+	function validate(){
+		var max = parseInt(document.getElementById('endYear').value);
+		var min = parseInt(document.getElementById('startYear').value);
+		if(min > max){
+			alert('Year range should be positive, enter as (Start Year --> End Year)');
+		return false;
+		}
+		else {
+		}
+	}
 }
--- webapp.py
+++ webapp.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    return render_template('index.html') # 'index.html'
+    return render_template('index.html')
 
 @app.route("/p1")
 def render_page1():
     if "startYear" in request.args:
-        return render_template('page1.html', points = format_dict_as_graph(get_sector_data()), options = get_country_names()) # 'page1.html', points = format_dict_as_graph(get_sector_data())
+        return render_template('page1.html', points = format_dict_as_graph(get_sector_data()), options = get_country_names(), splineData = format_dict_as_spline_graph(get_total_emissions_change()))
     else:
         return render_template('page1.html', options = get_country_names())
 
@@ -46,14 +46,24 @@ def get_sector_data():
     otherS = 0.0
     total = 0.0
     for country in countries:
-        if country["Country"] == targetCountry and country["Year"] > startYear and country["Year"] < endYear:
+        if country["Country"] == targetCountry and country["Year"] >= startYear and country["Year"] <= endYear:
             power += country["Emissions"]["Sector"]["Power Industry"]
             buildings += country["Emissions"]["Sector"]["Buildings"]
             transport += country["Emissions"]["Sector"]["Transport"]
             otherI += country["Emissions"]["Sector"]["Other Industry"]
             otherS += country["Emissions"]["Sector"]["Other sectors"]
-        else: #ADD STATEMENT TO HANDLE START YEAR > END YEAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        elif country["Country"] == targetCountry and startYear > endYear: # Statement to handle if startYear > endYear (only count the start year's data)
+            if country["Year"] == startYear:
+                power += country["Emissions"]["Sector"]["Power Industry"]
+                buildings += country["Emissions"]["Sector"]["Buildings"]
+                transport += country["Emissions"]["Sector"]["Transport"]
+                otherI += country["Emissions"]["Sector"]["Other Industry"]
+                otherS += country["Emissions"]["Sector"]["Other sectors"]
+            else:
+                pass
+        else:
             pass
+
     if (endYear-startYear) > 0 and endYear > startYear:
         power = power/(endYear-startYear) #End year minus start year to get averages
         buildings = buildings/(endYear-startYear)
@@ -73,7 +83,6 @@ def get_sector_data():
         otherI = otherI
         otherS = otherS
         
-    
     total = power + buildings + transport + otherI + otherS # Express the data in terms of percentage distribution between the 5 sectors
     power = 100 * round((power/total), 2) 
     buildings = 100 * round((buildings/total), 2)
@@ -93,6 +102,60 @@ def format_dict_as_graph(data):
     graph_points = graph_points[:-2]
     print(graph_points)
     return graph_points
+    
+def get_total_emissions_change():
+    with open('emissions.json') as emissions_data:
+        countries = json.load(emissions_data)
+    targetCountry = request.args['targetCountry']
+    startYear = float(request.args['startYear'])
+    endYear = float(request.args['endYear'])
+    totalCarbonPerYear = 0.0
+    targetYear = 0
+    totalCarbon = 0.0
+    emissionsPerYear = {}
+    for country in countries:
+        if country["Country"] == targetCountry and country["Year"] >= startYear and country["Year"] <= endYear and startYear != endYear:
+            totalCarbonPerYear = (country["Emissions"]["Type"]["CO2"] + country["Emissions"]["Type"]["N2O"] + country["Emissions"]["Type"]["CH4"])
+            targetYear = country["Year"]
+            emissionsPerYear.update({targetYear: totalCarbonPerYear})
+            totalCarbon = totalCarbon + totalCarbonPerYear
+            totalCarbonPerYear = 0.0
+            targetYear = 0
+        elif country["Country"] == targetCountry and startYear > endYear:# if the user inputs a start year thats greater than the end year, then flip the start and end years to make the function valid
+            newEndYear = startYear 
+            newStartYear = endYear
+            if country["Country"] == targetCountry and country["Year"] >= newStartYear and country["Year"] <= newEndYear and startYear != endYear:
+                totalCarbonPerYear = (country["Emissions"]["Type"]["CO2"] + country["Emissions"]["Type"]["N2O"] + country["Emissions"]["Type"]["CH4"])
+                targetYear = country["Year"]
+                emissionsPerYear.update({targetYear: totalCarbonPerYear})
+                totalCarbon = totalCarbon + totalCarbonPerYear
+                totalCarbonPerYear = 0.0
+                targetYear = 0
+            else:
+                pass
+        elif country["Country"] == targetCountry and startYear == endYear: #If the user inputs a year range of 0, then add 5 to the end year to make the graph readable
+            endYear = endYear + 5
+            if country["Country"] == targetCountry and country["Year"] >= startYear and country["Year"] <= endYear:
+                totalCarbonPerYear = (country["Emissions"]["Type"]["CO2"] + country["Emissions"]["Type"]["N2O"] + country["Emissions"]["Type"]["CH4"])
+                targetYear = country["Year"]
+                emissionsPerYear.update({targetYear: totalCarbonPerYear})
+                totalCarbon = totalCarbon + totalCarbonPerYear
+                totalCarbonPerYear = 0.0
+                targetYear = 0
+            else:
+                pass
+        else:
+            pass
+    return emissionsPerYear
+    
+def format_dict_as_spline_graph(data):
+    graphPoints = ""
+    for key in data:
+        # {x: new Date(2012, 0), y: 2009000}, 
+        graphPoints = graphPoints + Markup('{x: new Date(' + str(key) + ', 0), y: ' + str((data[key])) + '}, ')
+    graphPoints = graphPoints[:-2]
+    print(graphPoints)
+    return graphPoints
     
 
 if __name__ == '__main__':
